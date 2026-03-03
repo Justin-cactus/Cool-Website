c90b4e2889d2c5174fb7b622353eebb7efb3bad0
Justin-cactus <60158470+Justin-cactus@users.noreply.github.com>
2026-03-02 18:34:55 -0600

    Move projects into script

---
+++ js/project.js
@@ -0,0 +1,20 @@
+const projects = [
+  {
+    emoji: "🔵",
+    title: "Project One",
+    desc: "Placeholder?",
+    link: "https://github.com/yourusername"
+  },
+  {
+    emoji: "🟠",
+    title: "Project Two",
+    desc: "Placeholder 2?",
+    link: "https://github.com/yourusername"
+  },
+  {
+    emoji: "🟢",
+    title: "My little Webpage?",
+    desc: "Source Code for this website.",
+    link: "https://github.com/Justin-cactus/Cool-Website"
+  }
+];
